f8bbaed01cf9c7c8c618fd6821927c23224c593a
cs-shivaji-kolape <skolape@fortinet.com>
2023-11-08 15:12:23 +0530

    Added copyrights contents

--- radware-alteon/connector.py
+++ radware-alteon/connector.py
@@ -1,3 +1,10 @@
+""""
+Copyright start
+MIT License
+Copyright (c) 2023 Fortinet Inc
+Copyright end
+"""
+
 from connectors.core.connector import get_logger, ConnectorError, Connector
 
 from .operations import supported_operations, health_check
